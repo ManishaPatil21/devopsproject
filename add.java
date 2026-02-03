9ab8d8f552eb7706bf144c3b760d50d9a6741907
Manisha <manishap9766@gmail.com>
2026-02-03 13:09:40 +0530

    Initial commit / added all project files

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add {
+    public int addNo (int x, int y){
+        int sum = 0;
+        sum = x + y;
+        System.out.println("sum is " + sum);
+        return sum;
+    }
+    
+}
